c101da0840681a856f56d6bf0ebae6a65d1181e9
kkkunny <1401289859@qq.com>
2024-10-27 10:29:05 +0800

    支持返回图片

--- handler/chat_completions.go
+++ handler/chat_completions.go
@@ -96,143 +96,153 @@ func ChatCompletions(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !req.Stream {
-		var reply string
-		var tokenCount uint64
-		for msg := range chatResp.Stream {
-			switch msg.Type {
-			case api.StreamMessageTypeError:
-				config.Logger.Error(err)
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				return
-			case api.StreamMessageTypeFinalAnswer:
-				if msg.Text != nil {
-					reply = *msg.Text
-				}
-				break
-			case api.StreamMessageTypeStream:
-				tokenCount++
-			case api.StreamMessageTypeStatus:
-			default:
-				config.Logger.Warnf("unknown stream msg type `%s`", msg.Type)
+	handler := stlval.Ternary(req.Stream, chatCompletionsWithStream, chatCompletionsNoStream)
+	err = handler(w, msgID, convInfo, chatResp)
+	if err != nil {
+		config.Logger.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func chatCompletionsNoStream(w http.ResponseWriter, msgID string, convInfo *api.ConversationInfoResponse, resp *api.ChatConversationResponse) error {
+	var tokenCount uint64
+	var contents []openai.ChatMessagePart
+	for msg := range resp.Stream {
+		switch msg.Type {
+		case api.StreamMessageTypeError:
+			return msg.Error
+		case api.StreamMessageTypeFinalAnswer:
+			if stlval.DerefPtrOr(msg.Text) != "" {
+				contents = append(contents, openai.ChatMessagePart{
+					Type: openai.ChatMessagePartTypeText,
+					Text: *msg.Text,
+				})
 			}
-		}
-
-		data, err := json.Marshal(&openai.ChatCompletionResponse{
-			ID:      msgID,
-			Object:  "chat.completion",
-			Created: time.Now().Unix(),
-			Model:   convInfo.Model,
-			Choices: []openai.ChatCompletionChoice{
-				{
-					Index: 0,
-					Message: openai.ChatCompletionMessage{
-						Role:    "assistant",
-						Content: reply,
-					},
-					FinishReason: "stop",
-				},
-			},
-			Usage: openai.Usage{
-				PromptTokens:     0,
-				CompletionTokens: int(tokenCount),
-				TotalTokens:      int(tokenCount),
-			},
-		})
-		if err != nil {
-			config.Logger.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		_, err = fmt.Fprint(w, string(data))
-		if err != nil {
-			config.Logger.Error(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			return
-		}
-	} else {
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Connection", "keep-alive")
-		w.Header().Set("Transfer-Encoding", "chunked")
-
-		flusher := w.(http.Flusher)
-
-		for msg := range chatResp.Stream {
-			switch msg.Type {
-			case api.StreamMessageTypeError:
-				config.Logger.Error(err)
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				return
-			case api.StreamMessageTypeFinalAnswer:
-				data, err := json.Marshal(&openai.ChatCompletionStreamResponse{
-					ID:      msgID,
-					Object:  "chat.completion",
-					Created: time.Now().Unix(),
-					Model:   convInfo.Model,
-					Choices: []openai.ChatCompletionStreamChoice{
-						{
-							Index:        0,
-							FinishReason: "stop",
-						},
+			break
+		case api.StreamMessageTypeStream:
+			tokenCount++
+		case api.StreamMessageTypeFile:
+			if stlval.DerefPtrOr(msg.MIME) == "image/webp" && stlval.DerefPtrOr(msg.SHA) != "" {
+				contents = append(contents, openai.ChatMessagePart{
+					Type: openai.ChatMessagePartTypeImageURL,
+					ImageURL: &openai.ChatMessageImageURL{
+						Detail: openai.ImageURLDetailAuto,
+						URL:    fmt.Sprintf("%s/chat/conversation/%s/output/%s", consts.HuggingChatDomain, convInfo.ConversationID, *msg.SHA),
 					},
 				})
-				if err != nil {
-					config.Logger.Error(err)
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-					return
-				}
-				_, err = fmt.Fprint(w, "data: "+string(data)+"\n\n")
-				if err != nil {
-					config.Logger.Error(err)
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-					return
-				}
-				flusher.Flush()
-				_, err = fmt.Fprint(w, "data: [DONE]\n\n")
-				if err != nil {
-					config.Logger.Error(err)
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-					return
-				}
-				flusher.Flush()
-			case api.StreamMessageTypeStream:
-				var reply string
-				if msg.Token != nil {
-					reply = *msg.Token
-				}
-				data, err := json.Marshal(&openai.ChatCompletionStreamResponse{
-					ID:      msgID,
-					Object:  "chat.completion",
-					Created: time.Now().Unix(),
-					Model:   convInfo.Model,
-					Choices: []openai.ChatCompletionStreamChoice{
-						{
-							Index: 0,
-							Delta: openai.ChatCompletionStreamChoiceDelta{
-								Role:    "assistant",
-								Content: strings.TrimRight(reply, "\u0000"),
-							},
-						},
-					},
-				})
-				if err != nil {
-					config.Logger.Error(err)
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-					return
-				}
-				_, err = fmt.Fprint(w, "data: "+string(data)+"\n")
-				if err != nil {
-					config.Logger.Error(err)
-					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-					return
-				}
-				flusher.Flush()
-			case api.StreamMessageTypeStatus:
-			default:
-				config.Logger.Warnf("unknown stream msg type `%s`", msg.Type)
 			}
+		case api.StreamMessageTypeStatus, api.StreamMessageTypeTool:
+		default:
+			config.Logger.Warnf("unknown stream msg type `%s`", msg.Type)
 		}
 	}
+
+	var reply string
+	if len(contents) == 1 && contents[0].Type == openai.ChatMessagePartTypeText {
+		reply = contents[0].Text
+		contents = nil
+	}
+	data, err := json.Marshal(&openai.ChatCompletionResponse{
+		ID:      msgID,
+		Object:  "chat.completion",
+		Created: time.Now().Unix(),
+		Model:   convInfo.Model,
+		Choices: []openai.ChatCompletionChoice{
+			{
+				Index: 0,
+				Message: openai.ChatCompletionMessage{
+					Role:         "assistant",
+					Content:      reply,
+					MultiContent: contents,
+				},
+				FinishReason: "stop",
+			},
+		},
+		Usage: openai.Usage{
+			PromptTokens:     0,
+			CompletionTokens: int(tokenCount),
+			TotalTokens:      int(tokenCount),
+		},
+	})
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = fmt.Fprint(w, string(data))
+	return err
+}
+
+func chatCompletionsWithStream(w http.ResponseWriter, msgID string, convInfo *api.ConversationInfoResponse, resp *api.ChatConversationResponse) error {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
+	flusher := w.(http.Flusher)
+
+	for msg := range resp.Stream {
+		switch msg.Type {
+		case api.StreamMessageTypeError:
+			return msg.Error
+		case api.StreamMessageTypeFinalAnswer:
+			data, err := json.Marshal(&openai.ChatCompletionStreamResponse{
+				ID:      msgID,
+				Object:  "chat.completion",
+				Created: time.Now().Unix(),
+				Model:   convInfo.Model,
+				Choices: []openai.ChatCompletionStreamChoice{
+					{
+						Index:        0,
+						FinishReason: "stop",
+					},
+				},
+			})
+			if err != nil {
+				return err
+			}
+			_, err = fmt.Fprint(w, "data: "+string(data)+"\n\n")
+			if err != nil {
+				return err
+			}
+			flusher.Flush()
+			_, err = fmt.Fprint(w, "data: [DONE]\n\n")
+			if err != nil {
+				return err
+			}
+			flusher.Flush()
+		case api.StreamMessageTypeStream:
+			var reply string
+			if msg.Token != nil {
+				reply = *msg.Token
+			}
+			data, err := json.Marshal(&openai.ChatCompletionStreamResponse{
+				ID:      msgID,
+				Object:  "chat.completion",
+				Created: time.Now().Unix(),
+				Model:   convInfo.Model,
+				Choices: []openai.ChatCompletionStreamChoice{
+					{
+						Index: 0,
+						Delta: openai.ChatCompletionStreamChoiceDelta{
+							Role:    "assistant",
+							Content: strings.TrimRight(reply, "\u0000"),
+						},
+					},
+				},
+			})
+			if err != nil {
+				return err
+			}
+			_, err = fmt.Fprint(w, "data: "+string(data)+"\n")
+			if err != nil {
+				return err
+			}
+			flusher.Flush()
+		case api.StreamMessageTypeStatus, api.StreamMessageTypeTool, api.StreamMessageTypeFile:
+		default:
+			config.Logger.Warnf("unknown stream msg type `%s`", msg.Type)
+		}
+	}
+	return nil
 }
--- internal/api/api.go
+++ internal/api/api.go
@@ -288,14 +288,14 @@ func (api *Api) ConversationInfo(ctx context.Context, req *ConversationInfoReque
 }
 
 type ChatConversationRequest struct {
-	ConversationID string `json:"-"`
-	Files          []any  `json:"files,omitempty"`
-	ID             string `json:"id"`
-	Inputs         string `json:"inputs"`
-	IsContinue     bool   `json:"is_continue"`
-	IsRetry        bool   `json:"is_retry"`
-	WebSearch      bool   `json:"web_search"`
-	Tools          []any  `json:"tools"`
+	ConversationID string   `json:"-"`
+	Files          []any    `json:"files,omitempty"`
+	ID             string   `json:"id"`
+	Inputs         string   `json:"inputs"`
+	IsContinue     bool     `json:"is_continue"`
+	IsRetry        bool     `json:"is_retry"`
+	WebSearch      bool     `json:"web_search"`
+	Tools          []string `json:"tools"`
 }
 
 type ChatConversationResponse struct {
@@ -309,26 +309,58 @@ const (
 	StreamMessageTypeStream      StreamMessageType = "stream"
 	StreamMessageTypeFinalAnswer StreamMessageType = "finalAnswer"
 	StreamMessageTypeError       StreamMessageType = "error"
+	StreamMessageTypeTool        StreamMessageType = "tool"
+	StreamMessageTypeFile        StreamMessageType = "file"
+)
+
+type StreamMessageSubType string
+
+const (
+	StreamMessageSubTypeCall   StreamMessageSubType = "call"
+	StreamMessageSubTypeEta    StreamMessageSubType = "eta"
+	StreamMessageSubTypeResult StreamMessageSubType = "result"
 )
 
 type StreamMessageStatus string
 
 const (
-	StreamMessageStatusStarted StreamMessageStatus = "started"
-	StreamMessageStatusTitle   StreamMessageStatus = "title"
+	StreamMessageStatusStarted   StreamMessageStatus = "started"
+	StreamMessageStatusTitle     StreamMessageStatus = "title"
+	StreamMessageStatusSuccess   StreamMessageStatus = "success"
+	StreamMessageStatusKeepAlive StreamMessageStatus = "keepAlive"
 )
 
 type StreamMessage struct {
-	Type    StreamMessageType    `json:"type"`
-	Status  *StreamMessageStatus `json:"status,omitempty"`  // only StreamMessageTypeStatus
-	Token   *string              `json:"token,omitempty"`   // only StreamMessageTypeStream
-	Text    *string              `json:"text,omitempty"`    // only StreamMessageTypeFinalAnswer
-	Message *string              `json:"message,omitempty"` // only StreamMessageTypeStatus && StreamMessageStatusTitle
-	Error   error                `json:"-"`                 // only StreamMessageTypeError
+	Type    StreamMessageType      `json:"type"`
+	SubType *StreamMessageSubType  `json:"subtype,omitempty"` // only StreamMessageTypeTool
+	UUID    *string                `json:"uuid,omitempty"`    // only StreamMessageTypeTool
+	Eta     *float64               `json:"eta,omitempty"`     // only StreamMessageTypeTool && StreamMessageSubTypeEta
+	Call    *StreamMessageToolCall `json:"call,omitempty"`    // only StreamMessageTypeTool && (StreamMessageSubTypeCall || StreamMessageSubTypeResult)
+	Status  *StreamMessageStatus   `json:"status,omitempty"`  // only StreamMessageTypeStatus || (StreamMessageTypeTool && StreamMessageSubTypeResult)
+	Token   *string                `json:"token,omitempty"`   // only StreamMessageTypeStream
+	Text    *string                `json:"text,omitempty"`    // only StreamMessageTypeFinalAnswer
+	Message *string                `json:"message,omitempty"` // only StreamMessageTypeStatus && StreamMessageStatusTitle
+	Error   error                  `json:"-"`                 // only StreamMessageTypeError
+	Name    *string                `json:"name,omitempty"`    // only StreamMessageTypeFile
+	SHA     *string                `json:"sha,omitempty"`     // only StreamMessageTypeFile
+	MIME    *string                `json:"mime,omitempty"`    // only StreamMessageTypeFile
+}
+
+type StreamMessageToolCall struct {
+	Name       string                     `json:"name"`
+	Parameters StreamMessageToolParameter `json:"parameters"`
+}
+
+type StreamMessageToolParameter struct {
+	Prompt string `json:"prompt"`
+	Width  string `json:"width"`
+	Height string `json:"height"`
 }
 
 func (api *Api) ChatConversation(ctx context.Context, req *ChatConversationRequest) (*ChatConversationResponse, error) {
-	req.Tools = make([]any, 0)
+	if len(req.Tools) == 0 {
+		req.Tools = make([]string, 0)
+	}
 	reqBody, err := json.Marshal(req)
 	if err != nil {
 		return nil, err
